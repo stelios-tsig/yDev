c1f7bed1dbaee5220cfdb59361ce21530685f3f4
stelios-tsig <steliostsigapp@gmail.com>
2026-08-12 17:23:10 +0300

    classes added (Version , Comment, Rating schemas)

--- database.py
+++ database.py
@@ -6,6 +6,17 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./ydev.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # παράγει μια νέα συνεδρία βάσης δεδομένων
 
 Base = declarative_base()
+
+def get_db():
+    db= SessionLocal()
+    try:
+        yield db # Παράγει μια συνεδρία βάσης δεδομένων για χρήση σε ένα context manager.
+
+
+    finally:
+        db.close() # Το sesion κλείνει μετά την ολοκλήρωση της χρήσης του.
+
+        
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 import models
-from database import engine
+import schemas
+from database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "yDev. Welcome!"}
+
+
+@app.post("/technologies/", response_model=schemas.Technology)
+def create_technology(technology: schemas.TechnologyCreate, db: Session = Depends(get_db)):
+    db_technology = models.Technology(name=technology.name)
+    db.add(db_technology)
+    db.commit()
+    db.refresh(db_technology)
+    return db_technology
