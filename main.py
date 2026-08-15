4869afb83901c2953300ee2a15515409bb704aa7
stelios-tsig <steliostsigapp@gmail.com>
2026-08-15 16:36:24 +0300

    Get tab Technology created

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 import models
 import schemas
 from database import engine, get_db
+from typing import List
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -13,6 +14,8 @@ app = FastAPI()
 def read_root():
     return {"message": "yDev. Welcome!"}
 
+#Δημιουργία ενός νέου τεχνολογικού αντικειμένου
+#Δοκιμή με χρήση του FastAPI και SQLAlchemy για την αποθήκευση ενός νέου αντικειμένου τεχνολογίας στη βάση δεδομένων.
 
 @app.post("/technologies/", response_model=schemas.Technology)
 def create_technology(technology: schemas.TechnologyCreate, db: Session = Depends(get_db)):
@@ -21,3 +24,8 @@ def create_technology(technology: schemas.TechnologyCreate, db: Session = Depend
     db.commit()
     db.refresh(db_technology)
     return db_technology
+
+@app.get("/technologies/", response_model=List[schemas.Technology])
+def read_technologies(skip: int = 0, limit: int = 100, db:Session = Depends(get_db)):
+    technologies = db.query(models.Technology).offset(skip).limit(limit).all()
+    return technologies
